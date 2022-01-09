5111b84cbdce4caec3ed4075261dd310436ceb71
Isaacjuega <94757601+Isaacjuega@users.noreply.github.com>
2022-01-08 20:59:19 -0600

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,23 @@
+class launcher{
+    constructor(x,y,w,h)
+	{
+		var options={
+			isStatic:true			
+			}
+		this.x=x;
+		this.y=y;
+		this.w=w
+		this.h=h
+		this.body=Bodies.rectangle(x, y, w, h , options);
+ 		World.add(world, this.body);
+
+	}
+    fly(){
+        this.launcher.bodyA=null;
+    }
+    
+}
+
+function attach(){
+
+    }
